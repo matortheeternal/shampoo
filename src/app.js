7bae767bf7e8330cc42a4824c05aa71fc5dbf5a7
Mator <why101@msn.com>
2017-03-02 16:33:25 -0800

    added main view state and controller

--- src/app.js
+++ src/app.js
@@ -31,6 +31,19 @@ var testGetGlobal = function() {
     console.log("Failed to get exception information: " + e);
   }
   }
+};
+
+var LogXELibBuffer = function() {
+  console.log(xelib.GetBuffer());
+};
+
+var GetAndFlushBuffer = function() {
+  var log = xelib.GetBuffer();
+  if (log) {
+    xelib.FlushBuffer();
+    return log + "\n";
+  }
+  return "";
 }
 
 var ngapp = angular.module('shampoo', [
@@ -253,6 +266,74 @@ ngapp.service('errorsFactory', function() {
     ];
   }
 });
+
+ngapp.config(['$stateProvider', function ($stateProvider) {
+  $stateProvider.state('base.main', {
+    templateUrl: 'partials/main.html',
+    controller: 'mainController',
+    url: '/main'
+  });
+}]);
+
+ngapp.controller('mainController', function ($scope, $timeout, errorsFactory) {
+  $scope.loaded = false;
+  $scope.log = GetAndFlushBuffer();
+  $scope.checkedPlugins = 0;
+  $scope.plugins = [];
+  $scope.errors = [];
+  $scope.groupedErrors = errorsFactory.errorTypes();
+
+  $scope.spinnerOpts = {
+    lines: 17, // The number of lines to draw
+    length: 0, // The length of each line
+    width: 12, // The line thickness
+    radius: 50, // The radius of the inner circle
+    scale: 2, // Scales overall size of the spinner
+    corners: 0, // Corner roundness (0..1)
+    color: '#000', // #rgb or #rrggbb or array of colors
+    opacity: 0.05, // Opacity of the lines
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    speed: 0.9, // Rounds per second
+    trail: 70, // Afterglow percentage
+    fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+    zIndex: 2, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    position: 'relative' // Element positioning
+  };
+
+  $scope.checkForErrors = function() {
+    // TODO
+  };
+
+  $scope.getPlugins = function() {
+    var pluginIds = xelib.GetElements(0);
+    console.log(pluginIds);
+    $scope.plugins = pluginIds.map(function(_id) {
+      return {
+        _id: _id,
+        filename: xelib.Name(_id),
+        status: "Queued"
+      }
+    });
+    $scope.checkForErrors();
+  };
+
+  $scope.checkIfLoaded = function() {
+    $scope.log = $scope.log + GetAndFlushBuffer();
+    if (xelib.GetLoaderDone()) {
+      $scope.loaded = true;
+      $scope.getPlugins();
+    } else {
+      $timeout($scope.checkIfLoaded, 500);
+    }
+  };
+
+  $scope.checkIfLoaded();
 });
 
 ngapp.service('formUtils', function() {
