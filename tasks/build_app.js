efadecc246720059a408e4b60976f5016a6e0ad4
Will Bennion <will.bennion@outlook.com>
2017-05-14 01:02:43 +0100

    Changed CSS preprocessor from LESS to SASS

--- tasks/build_app.js
+++ tasks/build_app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
 var plumber = require('gulp-plumber');
@@ -20,10 +20,10 @@ gulp.task('bundle', function () {
     ]);
 });
 
-gulp.task('less', function () {
+gulp.task('sass', function () {
     return gulp.src(srcDir.path('stylesheets/*'))
         .pipe(plumber())
-        .pipe(less())
+        .pipe(sass())
         .pipe(gulp.dest(destDir.path('stylesheets')));
 });
 
@@ -45,9 +45,9 @@ gulp.task('watch', function () {
     watch('src/**/*.js', batch(function (events, done) {
         gulp.start('bundle', beepOnError(done));
     }));
-    watch('src/**/*.less', batch(function (events, done) {
-        gulp.start('less', beepOnError(done));
+    watch('src/**/*.scss', batch(function (events, done) {
+        gulp.start('sass', beepOnError(done));
     }));
 });
 
-gulp.task('build', ['bundle', 'less', 'environment']);
+gulp.task('build', ['bundle', 'sass', 'environment']);
