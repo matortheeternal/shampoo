8ff8a69b90a07f28282dca3f748545ee4eb27983
Mator <why101@msn.com>
2017-06-08 00:16:02 -0700

    created helper functions for navmesh resolution

--- src/Services/xelibService.js
+++ src/Services/xelibService.js
@@ -78,5 +78,81 @@ export default function(ngapp, xelib) {
                 console.log(e);
             }
         };
+
+        this.withElement = function(handle, path, callback) {
+            var element = xelib.GetElement(handle, path);
+            try {
+                callback(element);
+            } finally {
+                xelib.Release(element);
+            }
+        };
+
+        this.withElements = function(handle, path, callback) {
+            var elements = xelib.GetElements(handle, path);
+            try {
+                callback(elements);
+            } finally {
+                elements.forEach(xelib.Release);
+            }
+        };
+
+        this.withLinksTo = function(handle, path, callback) {
+            var element = xelib.GetLinksTo(handle, path);
+            try {
+                callback(element);
+            } finally {
+                xelib.Release(element);
+            }
+        };
+
+        this.MoveVerticesUnderground = function(handle) {
+            service.withElement(handle, 'NVNM\\Vertices', function(vertices) {
+                for (var i = 0; i < xelib.ElementCount(vertices); i++) {
+                    let z = xelib.GetFloatValue(vertices, `[${i}]`);
+                    xelib.SetFloatValue(vertices, `[${i}]`, z - 30000.0);
+                }
+            });
+        };
+
+        this.RemoveEdgeLinkFlags = function(triangle) {
+            var flags = xelib.GetUIntValue(triangle, `Flags`);
+            if (flags & 2048) {
+                xelib.SetIntValue(triangle, `Edge 0-1`, -1);
+            }
+            if (flags & 1024) {
+                xelib.SetIntValue(triangle, `Edge 1-2`, -1);
+            }
+            if (flags & 512) {
+                xelib.SetIntValue(triangle, `Edge 2-0`, -1);
+            }
+            if (flags >= 512) {
+                xelib.SetUIntValue(triangle, `Flags`, flags % 512);
+            }
+        };
+
+        this.RemoveEdgeLinks = function(handle, form) {
+            var currentForm = xelib.GetFormID(handle);
+            service.withElements(handle, 'NVNM\\Edge Links', function(edgeLinks) {
+                for (let i = edgeLinks.length - 1; i >= 0; i--) {
+                    if (form && xelib.GetUIntValue(edgeLinks[i], 'Mesh') != form) continue;
+                    service.withLinksTo(edgeLinks[i], 'Mesh', function(mesh) {
+                        let triangle = xelib.GetIntValue(edgeLinks[i], 'Triangle');
+                        service.withElement(mesh, `NVNM\\Triangles\\[${triangle}]`, function(meshTriangle) {
+                            service.RemoveEdgeLinkFlags(meshTriangle);
+                        });
+                        if (!form) service.RemoveEdgeLinks(mesh, currentForm);
+                    });
+                    xelib.RemoveElement(edgeLinks[i]);
+                }
+            });
+        };
+
+        this.UpdateMinMaxZ = function(handle) {
+            var minZ = xelib.GetFloatValue(handle, 'NVNM\\Min Z');
+            xelib.SetFloatValue(handle, 'NVNM\\Min Z', minZ - 30000.0);
+            var maxZ = xelib.GetFloatValue(handle, 'NVNM\\Max Z');
+            xelib.SetFloatValue(handle, 'NVNM\\Max Z', maxZ - 30000.0);
+        };
     });
 }
